af15e5325c1d354f9bb51c3a2438e189da4de0cd
Michael Voříšek <mvorisek@mvorisek.cz>
2023-12-12 13:09:11 +0100

    upgrade 1:1 vanilla/htmlawed to 2.2.15

--- HtmLawed/HtmLawedVanillaWrapper.php
+++ HtmLawed/HtmLawedVanillaWrapper.php
@@ -1,62 +1,36 @@
 <?php
 /**
- * HtmLawed is used here to provide protection against XSS attacks with Editor
- * input - see the `Field->xss()` method. The Vanilla forums wrapper is used
- * to provide sensible defaults and a clean interface for HtmLawed.
- *
- * Changes:
- *
- *  * Add `DataTables/HtmLawed` namespace to this and htmLawed - this is to ensure
- *    that if htmLawed is included by any other aspect of the site it will not
- *    result in a conflict.
- *  * Use the OOP version of htmLawed (required a single updated to call it) to
- *    make the namespacing relatively easy.
- *  * Change the name of the Vanilla class so it don't conflict with the
- *    htmLawed OOP class
- *  * Update all `htmLawed::` references to `\DataTables\HtmLawed\HtmLawed::` in
- *    the htmLawed file (to allow callbacks to operate correctly)
- *  * Updated Vanilla wrapper to operate on PHP 5.3
- *
- * HtmLawed:
- *   http://www.bioinformatics.org/phplabware/internal_utilities/htmLawed/
- *   Copyright: Santosh Patnaik
- *   License: Dual licensed with LGPL 3 and GPL 2+
- *
- * Vanilla wrapper for HtmLawed:
- *   https://github.com/vanilla/htmlawed/
- *   Author: Todd Burry <todd@vanillaforums.com>
- *   Copyright: 2009-2014 Vanilla Forums Inc.
- *   License: LGPL-3.0
+ * @author Todd Burry <todd@vanillaforums.com>
+ * @copyright 2009-2014 Vanilla Forums Inc.
+ * @license LGPL-3.0
  */
-
-namespace DataTables\HtmLawed;
 
 /**
  * A class wrapper for the htmLawed library.
  */
-class HtmLawedVanillaWrapper
-{
+class Htmlawed {
     /// Methods ///
 
-    public static $defaultConfig = array(
-        'anti_link_spam' => array('`.`', ''),
+    public static $defaultConfig = [
+        'anti_link_spam' => ['`.`', ''],
         'balance' => 1,
         'cdata' => 3,
+        'safe' => 1,
         'comment' => 1,
-        'css_expression' => 1,
-        'deny_attribute' => 'on*',
+        'css_expression' => 0,
+        'deny_attribute' => 'on*,style',
         'direct_list_nest' => 1,
-        'elements' => '*-applet-form-input-textarea-iframe-script-style-embed-object',
-        'keep_bad' => 1,
+        'elements' => '*-applet-button-form-input-textarea-iframe-script-style-embed-object',
+        'keep_bad' => 0,
         'schemes' => 'classid:clsid; href: aim, feed, file, ftp, gopher, http, https, irc, mailto, news, nntp, sftp, ssh, telnet; style: nil; *:file, http, https', // clsid allowed in class
         'unique_ids' => 0,
         'valid_xhtml' => 0,
-    );
+    ];
 
-    public static $defaultSpec = array(
+    public static $defaultSpec = [
         'object=-classid-type, -codebase',
         'embed=type(oneof=application/x-shockwave-flash)'
-    );
+    ];
 
     /**
      * Filters a string of html with the htmLawed library.
@@ -64,13 +38,12 @@ class HtmLawedVanillaWrapper
      * @param string $html The text to filter.
      * @param array|null $config Config settings for the array.
      * @param string|array|null $spec A specification to further limit the allowed attribute values in the html.
-     *
      * @return string Returns the filtered html.
-     *
      * @see http://www.bioinformatics.org/phplabware/internal_utilities/htmLawed/htmLawed_README.htm
      */
-    public static function filter($html, array $config = null, $spec = null)
-    {
+    public static function filter($html, array $config = null, $spec = null) {
+        require_once __DIR__.'/htmLawed/htmLawed.php';
+
         if ($config === null) {
             $config = self::$defaultConfig;
         }
@@ -83,21 +56,20 @@ class HtmLawedVanillaWrapper
             $spec = static::$defaultSpec;
         }
 
-        return HtmLawed::hl($html, $config, $spec);
+        return htmLawed($html, $config, $spec);
     }
 
 
     /**
      * Filter a string of html so that it can be put into an rss feed.
      *
-     * @param string $html The html text to fitlter.
+     * @param $html The html text to fitlter.
      * @return string Returns the filtered html.
-     * @see HtmLawed::filter().
+     * @see Htmlawed::filter().
      */
-    public static function filterRSS($html)
-    {
+    public static function filterRSS($html) {
         $config = array(
-            'anti_link_spam' => array('`.`', ''),
+            'anti_link_spam' => ['`.`', ''],
             'comment' => 1,
             'cdata' => 3,
             'css_expression' => 1,
@@ -105,7 +77,7 @@ class HtmLawedVanillaWrapper
             'elements' => '*-applet-form-input-textarea-iframe-script-style-object-embed-comment-link-listing-meta-noscript-plaintext-xmp',
             'keep_bad' => 0,
             'schemes' => 'classid:clsid; href: aim, feed, file, ftp, gopher, http, https, irc, mailto, news, nntp, sftp, ssh, telnet; style: nil; *:file, http, https', // clsid allowed in class
-            'valid_xml' => 2,
+            'valid_xhtml' => 1,
             'balance' => 1
         );
         $spec = static::$defaultSpec;
@@ -115,3 +87,4 @@ class HtmLawedVanillaWrapper
         return $result;
     }
 }
+ 
