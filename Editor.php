ef7a9cff90ca9e1a5424f4e32743ddbe4d84a42f
Allan Jardine <allan@datatables.net>
2024-03-08 11:38:41 +0000

    Release 2.3.2

--- Editor.php
+++ Editor.php
@@ -139,7 +139,7 @@ class Editor extends Ext
 	 */
 
 	/** @var string */
-	public $version = '2.3.1';
+	public $version = '2.3.2';
 
 	/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 	 * Private properties
