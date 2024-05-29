f725ab468aeead30fa991910ffd36d555c621593
Allan Jardine <allan@datatables.net>
2024-05-29 09:23:16 +0000

    Fix: Server-side processing with SQL Server would give an error when entering into the neutral sort (i.e. third click of a column header

--- Editor.php
+++ Editor.php
@@ -1669,6 +1669,13 @@ class Editor extends Ext
 				);
 			}
 		}
+
+		// Paging makes little sense without an ordering clause, so if there is
+		// no order to apply (possible in DT2 on the third click of a header)
+		// we apply the primary key as the ordering value.
+		if (! isset($http['order']) || count($http['order']) === 0) {
+			$query->order($this->_pkey[0] . ' asc');
+		}
 	}
 
 	private function _constructSearchBuilderConditions($query, $data)
